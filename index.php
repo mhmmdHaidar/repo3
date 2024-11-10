dc7b55ec3d1f83b75e0aacfe4248bb70ae2ba547
Mhmmad Haidar <mhmmdhaidar1212@gmail.com>
2024-11-10 21:22:04 +0700

    Menambahkan paragrap

--- index.php
+++ index.php
@@ -7,5 +7,7 @@
 </head>
 <body>
     <h2>Hello World</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam magnam, veniam maiores nostrum harum omnis earum architecto iusto nemo alias ipsam vel quo maxime iste quam veritatis cupiditate eveniet aut.</p>
 </body>
 </html>
